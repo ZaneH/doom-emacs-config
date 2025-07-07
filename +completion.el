c90ccc94c97d54711fc0d0c8857bab28a2afd86d
Zane Helton <zane99@me.com>
2025-07-07 16:14:24 -0400

    Improve completion delay time

--- +completion.el
+++ +completion.el
@@ -4,5 +4,14 @@
 (after! lsp-mode
   (setq lsp-completion-provider :capf))
 (after! company
-  (setq company-idle-delay 5
-        company-minimum-prefix-length 1))
+  (setq company-idle-delay 0.2
+        company-minimum-prefix-length 1)
+
+  ;; Use higher delay for certain modes
+  (add-hook 'markdown-mode-hook
+            (lambda () (setq-local company-idle-delay 5)))
+  (add-hook 'org-mode-hook
+            (lambda () (setq-local company-idle-delay 5)))
+  (add-hook 'text-mode-hook
+            (lambda () (setq-local company-idle-delay 5)))
+  )
